ccbb2284547885fd929d76bcf8f0f1450166f163
tumbulhasankadir-design <tumbulhasankadir@gmail.com>
2026-07-25 22:00:46 +0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -16,7 +16,6 @@ from stmol import showmol
 # -----------------------------------------
 # 1. ORTAM VE YOL AYARLARI
 # -----------------------------------------
-load_dotenv()
 current_dir = os.path.dirname(os.path.abspath(__file__))
 if current_dir not in sys.path:
     sys.path.insert(0, current_dir)
